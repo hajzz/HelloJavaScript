572d8927c0137264c224c1473a5b71a5a72e9f9c
Hajz <iovita.valentino@gmail.com>
2020-09-20 21:29:25 +0100

    Lesson 15
    Added cycles and conditions

--- Learning/movies.js
+++ Learning/movies.js
@@ -1,19 +1,38 @@
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-const personalMovieDB = {
+const personalMovieDB = {           //создаётся объект со свойствами
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     private: false
 };
+if (personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов")
+} else if (personalMovieDB.count >=10 && personalMovieDB.count <= 30){
+    alert("Вы классический зритель"); 
+} else if(personalMovieDB.count > 30){
+    alert("Вы киноман")
+} else {
+    alert("Произошла ошибка");
+}
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d= prompt('На сколько оцените его?', '');
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d= prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b; // обращение к переменной, присваивание значения и помещение туда ответа из промпт
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// for (let i = 0; i < 2; i++){            //функция будет повторяться 2 раза
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+//     if(a != null && b != null && a != '' && b != '' && a.length < 50 && b.length <50){ //Если условия соблюдены, то продолжаем
+//         personalMovieDB.movies[a] = b;
+//     }else {
+//         console.log('error');
+//         i--;            //добавляем еще один цикл, так как условия не соблюдены
+//     }
+// }
+// console.log(personalMovieDB);
