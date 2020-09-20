b2c27bd93eccecb61b1282858474a5c0fb20378e
Hajz <iovita.valentino@gmail.com>
2020-09-20 22:12:55 +0100

    Added second cycle method

--- Learning/movies.js
+++ Learning/movies.js
@@ -2,17 +2,17 @@ let numberOfFilms = prompt('Сколько фильмов вы уже посмо
 
 const personalMovieDB = {           //создаётся объект со свойствами
     count: numberOfFilms,
-    movies: {},
+    movies: [],
     actors: {},
     genres: [],
     private: false
 };
 if (personalMovieDB.count < 10){
-    alert("Просмотрено довольно мало фильмов")
+    alert("Просмотрено довольно мало фильмов");
 } else if (personalMovieDB.count >=10 && personalMovieDB.count <= 30){
     alert("Вы классический зритель"); 
 } else if(personalMovieDB.count > 30){
-    alert("Вы киноман")
+    alert("Вы киноман");
 } else {
     alert("Произошла ошибка");
 }
@@ -35,4 +35,27 @@ if (personalMovieDB.count < 10){
 //         i--;            //добавляем еще один цикл, так как условия не соблюдены
 //     }
 // }
-// console.log(personalMovieDB);
+
+let i = 0;
+while (i < 1){
+    i++;
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    if(a != null && a != '' && a.length < 50){ //Если условия соблюдены, то продолжаем
+        personalMovieDB.movies = [a];
+    }else {
+        console.log('error');
+        i--;
+    }
+}
+let i2 = 0;
+while (i2 < 1){
+    i2++;
+    const b = prompt('На сколько оцените его?', '');
+    if(b != null && b != '' && b.length <50){ //Если условия соблюдены, то продолжаем
+              personalMovieDB.movies = [b];
+          }else {
+              console.log('error');
+              i2--;
+          }
+}
+console.log(personalMovieDB);
