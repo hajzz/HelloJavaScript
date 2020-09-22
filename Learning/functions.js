9c72a135ff800304ccea95a0bb9bc7b6eef15b36
Hajz <iovita.valentino@gmail.com>
2020-09-22 21:12:23 +0100

    zero
    zero touch commit

--- Learning/functions.js
+++ Learning/functions.js
@@ -7,7 +7,20 @@ function calc(a, b ){           //Функция принимает аргуме
 console.log(calc(14,25));       // помещаем 14 и 25 в аргументы функции
 
 
+                                    
+showText ('Hi');                     //Function declaration.Вызываем функцию и помещаем аргумент
 function showText (text){
     console.log(text);              //Помещаем действие в функцию
 }
-showText ('Hi');                     //Вызываем функцию и помещаем аргумент
+
+
+const logger = function(a, b){      //Function expression. объявляется переменная и задаэтся функция
+    console.log ('hello');
+};
+logger();
+
+
+//Стрелочная функция
+const arrowFunction = (a, b) => a + b; //Создаём переменную с аргументами a,b помещаем туда функцию => a + b
+                 // const arrowFunction = (a, b) =>  { return a + b};
+                 // const arrowFunction = a => a + b;
