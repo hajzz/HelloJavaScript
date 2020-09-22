a83aba39503adb53f47dc4e131ef763938729061
Hajz <iovita.valentino@gmail.com>
2020-09-22 23:28:41 +0100

    added object with properties, learned how to log the properties separately into console
    Lesson 20

--- Learning/object.js
+++ Learning/object.js
@@ -4,3 +4,36 @@ let obj = {
     isSingle: true
 };
 alert (obj);
+
+
+const options = {         //создали объeкт со свойствами(ключами)
+    name: 'Hajz',
+    type: 'jpg',
+    size: '1024kb',
+    properties: {          //объект внутри объекта со свойствами
+        width: 4000,
+        height: 2000
+    }
+};
+
+// console.log(options ['properties'] ['width']);    
+//вывод значения свойства объекта properties в объекте options
+
+
+for (let keyOptions in options){        
+    // Задаётся переменная keyOptions которая будет выводить значения ключей
+    //При помощи  метода in ковыряемся в options. 
+    //цикл будет повторятся в зависимости от количества ключей
+    // console.log(`Option ${keyOptions} contain ${options[keyOptions]}`);   
+    //в keyOptions консоль веведет сами ключи, а options [keyOptions] выведет значения
+    if (typeof(options[keyOptions]) === 'object'){
+        //Проверяем options[keyOptions] если это объект
+        for (let keyProperties in options[keyOptions]) {
+            //задаём новую переменную внутри для свойства properties в options[keyOptions]
+            console.log(`Option ${keyProperties} contain ${options[keyOptions][keyProperties]}`);
+            //Выводим в консоль свойства объекта внутри объекта, если таковой имеется
+        }
+    } else {
+        console.log(`Option ${keyOptions} contain ${options [keyOptions]}`);
+    }
+    }
