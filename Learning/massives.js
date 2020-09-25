7b29fa02defe4f9df559ab0a3e0d4b19e4a981ba
Hajz <iovita.valentino@gmail.com>
2020-09-25 20:20:07 +0100

    Addition to last lesson
    Lesson 21

--- Learning/massives.js
+++ Learning/massives.js
@@ -1,8 +1,15 @@
-"use strict"
+"use strict";
 
 
 let arr = [1, 4, 5, 10, 3];
+arr.sort(arrangeNumbers);
+//в случае с числами будут выводиться сначала первые значения по чисел по возрастанию, НО читывая только по 1 числу.
+console.log(arr);
 
+function arrangeNumbers(a, b){
+    return a - b;
+}
+//этa функция расставляет числа по порядку
 
 // arr[11] = 105;             
 // console.log(arr.length);
@@ -25,5 +32,6 @@ const str = prompt('type in values separated by comma','');
 const inputValues = str.split(', ');
 //metod .split создаёт массив, в параметрах указываем разделитель
 inputValues.sort();
+//ковертирует в строку и сортирует по алфавитному порядку
 console.log(inputValues.join('; '));
 //метод .join конвертирует массив в строку. В параметрах указываем каким знаком будем разделять
