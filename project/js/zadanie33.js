830fb7adccc576d80fafefb42e4b6edfea195489
Valentin Iovita <iovita.valentino@gmail.com>
2020-12-18 22:56:30 +0000

    Switch to macbook;34 finished

--- project/js/zadanie33.js
+++ project/js/zadanie33.js
@@ -28,36 +28,70 @@ const movieDB = {
         "Скотт Пилигрим против..."
     ]
 };
-const confirmButton = document.getElementById('confirmButton');
-// console.log(confirmButton);
-const inputBox = document.getElementsByClassName('adding__input');
-// console.log(inputBox[0]);
- const trashButton = document.querySelectorAll('.delete'),
-       trashButtonParent = document.querySelector('.delete').parentElement;
- console.log(trashButton);
+const adv = document.querySelectorAll('.promo__adv'),
+    movieList = document.querySelector('.promo__interactive-list'),
+    addForm = document.querySelector('form.add'),
+    addFormInput = addForm.querySelector('.adding__input'),
+    formCheckox = addForm.querySelector('[type = "checkbox"]');
 
-confirmButton.addEventListener('click', (e) => {
+    // console.log(formCheckox.checked);
+
+const deleteAdv = (arr) => {
+    arr.forEach(item => {
+        item.remove();
+    });
+};
+
+const sortArray = (arr) => {
+    arr.sort();
+};
+
+
+function addMovieToList(films, parent) {
+    parent.innerHTML = '';
+
+    sortArray(films);
+
+    films.forEach((film, i) => {
+        parent.innerHTML += `<li class="promo__interactive-item">${i + 1} ${film}
+    <div class="delete"></div>
+</li>`;
+    });
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+            addMovieToList(films, parent);
+            console.log(movieDB.movies);
+        });
+    });
+}
+
+addForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    // inputBox.forEach((item) => {
-    //     inputBox = item;
-    // });
-    let newEntry = inputBox[0].value;
-    newEntry = String(newEntry);
-    if (newEntry.lengh >= 6){ 
-        newEntry.slice(-1,);
-        newEntry += '...';
-    // } else if(true){
-    //     movieDB.movies.push(inputBox[0].value);
+
+    let newFilm = addFormInput.value;
+    let favorite = formCheckox.checked;
+
+    if (favorite) {
+        console.log("Adding to favorites");
     }
-    console.log(newEntry);
-    // console.log(typeof(newEntry.lengh));
-});
-//adding movie to out database
-// console.log(inputBox[0].value);
-// console.log(inputBox);
 
-trashButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log('tarshbutton');
+    if (newFilm) {
+
+        if (newFilm.length > 5) {
+            newFilm = `${newFilm.substring(0, 6)}...`;
+        }
+        
+        movieDB.movies.push(newFilm);
+        console.log(movieDB.movies);
+        addMovieToList(movieDB.movies, movieList);
+    }
+
+
+    e.target.reset();
 });
 
+
+addMovieToList(movieDB.movies, movieList);
+deleteAdv(adv);
